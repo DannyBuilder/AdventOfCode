5c6f094fccef8773586390d6543868c3c919b52b
Daniel Kolev <d.kolev@student.tudelft.nl>
2025-12-06 12:13:24 +0200

    Day 2 Part 2

--- AdventOfCode/Day2.cs
+++ AdventOfCode/Day2.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode
             input = line.Split(',');
         }
 
-        public long part1()
+        public long Part1()
         {
             long answ = 0;
             for (int i = 0; i < input.Length; i++)
@@ -26,6 +26,67 @@ namespace AdventOfCode
             }
 
             return answ;
+        }
+
+        public long Part2()
+        {
+            
+            var ranges = new List<(long Start, long End)>();
+            long globalMax = 0;
+
+            foreach (var line in input)
+            {
+                var parts = line.Split('-');
+                long s = long.Parse(parts[0]);
+                long e = long.Parse(parts[1]);
+                ranges.Add((s, e));
+                if (e > globalMax) globalMax = e;
+            }
+            
+            var foundIds = new HashSet<long>();
+            int maxDigits = globalMax.ToString().Length;
+            
+            for (int L = 2; L <= maxDigits; L++)
+            {
+                for (int l = 1; l <= L / 2; l++)
+                {
+                    if (L % l == 0)
+                    {
+                        int k = L / l;
+                        long multiplier = 0;
+                        long blockValue = 1;
+                        long shift = (long)Math.Pow(10, l);
+
+                        for (int i = 0; i < k; i++)
+                        {
+                            multiplier += blockValue;
+                            if (i < k - 1) blockValue *= shift;
+                        }
+                        
+                        long minSeedLimit = (long)Math.Pow(10, l - 1);
+                        long maxSeedLimit = (long)Math.Pow(10, l) - 1;
+
+                        foreach (var range in ranges)
+                        {
+                            long minSeedCalc = (long)Math.Ceiling((double)range.Start / multiplier);
+                            long maxSeedCalc = (long)Math.Floor((double)range.End / multiplier);
+                            long startSeed = Math.Max(minSeedLimit, minSeedCalc);
+                            long endSeed = Math.Min(maxSeedLimit, maxSeedCalc);
+
+                            if (startSeed <= endSeed)
+                            {
+                                for (long seed = startSeed; seed <= endSeed; seed++)
+                                {
+                                    foundIds.Add(seed * multiplier);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return foundIds.Sum();
+            
         }
     }
 }
--- AdventOfCode/Main.cs
+++ AdventOfCode/Main.cs
@@ -12,7 +12,8 @@ namespace AdventOfCode
             Console.WriteLine("Day 1 - Part 2: " + day1.part2());
             */
             Day2 day2 = new Day2();
-            Console.WriteLine("Day 2 - Part 1: " + day2.part1());
+            Console.WriteLine("Day 2 - Part 1: " + day2.Part1());
+            Console.WriteLine("Day 2 - Part 2: " + day2.Part2());
             Console.ReadLine(); 
         }
     }
