e9a6fdfbdd790e953f9bb3b37f5977fd8831d26e
Daniel Kolev <d.kolev@student.tudelft.nl>
2025-12-09 21:52:59 +0200

    day 4 part 1

---
+++ AdventOfCode/Day4.cs
@@ -0,0 +1,74 @@
+namespace AdventOfCode;
+
+public class Day4
+{
+    private readonly List<string> _allLines = [];
+    private int[,] _dp;
+    
+    public Day4()
+    {
+        string? line;
+        while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+        {
+            _allLines.Add(line);
+        }
+
+        _dp = new int[_allLines.Count, _allLines[0].Length];
+        for(var i =0; i < _allLines.Count; i++){
+            for (var j = 0; j < _allLines[i].Length; j++)
+            {
+                if(_allLines[i][j] == '.')
+                {
+                    _dp[i, j] = 0;
+                }
+                else
+                {
+                    _dp[i, j] = 1;
+                }
+            }
+        }
+    }
+
+    public int Part1()
+    {
+        var answ = 0;
+        for(var i =0; i < _dp.GetLength(0); i++){
+            for (var j = 0; j < _dp.GetLength(1); j++)
+            {
+                if (_dp[i, j] == 1)
+                {
+                    if (Check(i, j))
+                    {
+                        answ++;
+                    }
+                }
+            }
+        }
+
+        return answ;
+    }
+    
+    private bool Check(int r, int c)
+    {
+        var neighborCount = 0;
+
+        int[] dRow = [-1, -1, -1,  0, 0,  1, 1, 1];
+        int[] dCol = [-1,  0,  1, -1, 1, -1, 0, 1];
+
+        for (var k = 0; k < 8; k++)
+        {
+            var newRow = r + dRow[k];
+            var newCol = c + dCol[k];
+            
+            if (newRow >= 0 && newRow < _dp.GetLength(0) && 
+                newCol >= 0 && newCol < _dp.GetLength(1))
+            {
+                if (_dp[newRow, newCol] == 1)
+                {
+                    neighborCount++;
+                }
+            }
+        }
+        return neighborCount < 4;
+    }
+}
--- AdventOfCode/Main.cs
+++ AdventOfCode/Main.cs
@@ -14,11 +14,13 @@ namespace AdventOfCode
             var day2 = new Day2();
             Console.WriteLine("Day 2 - Part 1: " + day2.Part1());
             Console.WriteLine("Day 2 - Part 2: " + day2.Part2());
-            */
             var day3 = new Day3();
             Console.WriteLine("Day 3 - Part 1: " +day3.Part1());
             Console.WriteLine("Day 3 - Part 2: " +day3.Part2());
             Console.ReadLine(); 
+            */
+            var day4 = new Day4();
+            Console.WriteLine("Day 4 - Part 4: " + day4.Part1());
         }
     }
 }
